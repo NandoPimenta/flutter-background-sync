593d1192301cdc4ac690cb2a1c16253b239a262b
Fernando H. Pimenta <fernandopimentadev@gmail.com>
2024-04-08 10:58:25 -0300

    get packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:package_firebase/package_firebase.dart';
 import 'package:package_module/package_module.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 import 'firebase_options.dart';
-
 import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
